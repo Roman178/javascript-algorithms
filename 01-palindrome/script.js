e2f1c2508b7bc33d68e372b8245492f6fb2b1594
Roman <ismagilov771@gmail.com>
2021-07-09 00:35:59 +0300

    second task.

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -7,26 +7,37 @@
  *
  * Напишите функцию palindrome(str), принимающую как аргумент строку.
  * Функция должна вернуть true, если строка — палиндром, и false, если нет.
- * 
+ *
  * Считайте, что на вход всегда передаётся слово: то есть знаков препинания
  * и пробелов в аргументе быть не может.
- * 
-*/
+ *
+ */
 
 function palindrome(str) {
-    // Напишите код здесь
+  const onlyCharsAndDigs = str
+    .replace(/[^A-Za-z0-9а-яА-Я\s]/g, "")
+    .replace(/\s+/g, "")
+    .toLowerCase();
+  const reversedStr = onlyCharsAndDigs
+    .split("")
+    .reverse()
+    .join("")
+    .toLowerCase();
+
+  return onlyCharsAndDigs === reversedStr;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(palindrome('топот')); // должно быть true
-console.log(palindrome('Saippuakivikauppias')); // true
-console.log(palindrome('привет')); // false
+console.log(palindrome("топот")); // должно быть true
+console.log(palindrome("Saippuakivikauppias")); // true
+console.log(palindrome("привет")); // false
+console.log(palindrome("прDDSивет? sdАВАЫsda. cds!@., $$./. uyuj")); // false
 
 /*
  * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
  * но пробелы и знаки препинания не учитывает. Например:
- * 
+ *
  * palindrome('О, лета тело!'); // true
- * 
-*/
+ *
+ */
--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -7,11 +7,24 @@
  * • кратные трём — на fizz;
  * • кратные пяти — на buzz;
  * • кратные и трём, и пяти одновременно — на fizzbuzz.
- * 
-*/
+ *
+ */
 
 function fizzBuzz(num) {
-    // Напишите код здесь
+  if (typeof num !== "number") return;
+  if (num < 1) return;
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+      continue;
+    } else {
+      console.log(i);
+      continue;
+    }
+    i % 3 === 0 ? console.log("fizz") : console.log(i);
+    i % 5 === 0 ? console.log("buzz") : console.log(i);
+  }
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
